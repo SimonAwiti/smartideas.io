f7e86c969efb9f056bb2949e018b124434bb77e5
simonawiti <awitisimon23@gmail.com>
2025-02-10 11:03:27 +0100

    webapp revamp

--- index.php
+++ index.php
@@ -1,114 +1,259 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<head>
-   <!-- basic -->
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>GoxLog.com</title>
-  
-   <!-- Bootstrap CSS -->
-   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-   <!-- style CSS -->
-   <link rel="stylesheet" type="text/css" href="css/style.css">
-   <!-- Responsive CSS -->
-   <link rel="stylesheet" href="css/responsive.css">
-   <!-- favicon -->
-   <link rel="icon" href="images/fevicon.png" type="image/gif" />
-   <!-- Scrollbar Custom CSS -->
-   <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
-   <!-- Tweaks for older IEs -->
-   <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-   <!-- Fonts -->
-   <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Poppins:400,700&display=swap" rel="stylesheet">
-   <!-- Owl Stylesheets -->
-   <link rel="stylesheet" href="css/owl.carousel.min.css">
-   <link rel="stylesheet" href="css/owl.theme.default.min.css">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
-</head>
+        <meta name="description" content="">
+        <meta name="author" content="Simon Awiti">
 
-<body>
-   <!-- Header Section Start -->
-   <div class="header_section">
-      <div class="container-fluid">
-         <div class="row">
-            <div class="col-sm-3 col-6">
-               <!-- Logo with SmartIdeas.io -->
-               <a class="logo" href="index.php"><h1 style="font-size: 45px;color: black;"><b>GoxLog.com</b></h1></a>
+        <title>Goxlog.com</title>
+
+        <!-- CSS FILES -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="css/bootstrap-icons.css" rel="stylesheet">
+
+        <link href="css/magnific-popup.css" rel="stylesheet">
+
+        <link href="css/templatemo-first-portfolio-style.css" rel="stylesheet">
+        
+    </head>
+    
+    <body>
+
+        <section class="preloader">
+            <div class="spinner">
+                <span class="spinner-rotate"></span>    
             </div>
-         </div>
-      </div><br>
-      <!-- Banner Section Start -->
-      <div class="banner_section layout_padding">
-         <div class="container">
-            <div id="main_slider" class="carousel slide" data-ride="carousel">
-               <div class="carousel-inner">
-                  <!-- First Carousel Item -->
-                  <div class="carousel-item active">
-                     <h1 class="banner_taital">Marketplace for Business Ideas</h1>
-                     <h3 class="banner_taital">Do you have a good business Idea?</h3>
-                     <div class="read_bt"></div>
-                     <div class="number_main">
-                        <div class="number_text">01</div><br>  
-                     </div>
-                     <!-- Button under the first slide with space -->
-                     <div class="text-center mt-4">
-                        <a href="allideas.php" class="btn btn-black" style="font-size: 18px; padding: 10px 30px; background-color: #ffffff">All Posted Ideas</a>
-                     </div>
-                  </div>
-                  <!-- Second Carousel Item -->
-                  <div class="carousel-item">
-                     <h2 class="banner_taital">Do you have a good business Idea?</h2>
-                     <h1 class="banner_taital">Start today by posting your business idea</h1>
-                     <div class="read_bt"></div>
-                     <div class="number_main">
-                        <div class="number_text">02</div><br>
-                     </div>
-                     <!-- Button under the second slide with space -->
-                     <div class="text-center mt-4">
-                        <a href="allideas.php" class="btn btn-black" style="font-size: 18px; padding: 10px 30px; background-color: #ffffff">All Posted Ideas</a>
-                     </div>
-                  </div>
-               </div>
-               <a class="carousel-control-prev" href="#main_slider" role="button" data-slide="prev">
-                  <i class="fa fa-angle-left"></i>
-               </a>
-               <a class="carousel-control-next" href="#main_slider" role="button" data-slide="next">
-                  <i class="fa fa-angle-right"></i>
-               </a>
+        </section>
+
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <a href="index.html" class="navbar-brand mx-auto mx-lg-0">Goxlog.com</a>
+
+                <div class="d-flex align-items-center d-lg-none">
+                    <i class="navbar-icon bi-telephone-plus me-3"></i>
+                    <a class="custom-btn btn" href="#section_5">
+                        +254798226640
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-lg-5">
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="#section_1">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="#section_2">About</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="login.html">Login</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="#section_5">Contact</a>
+                        </li>
+                    </ul>
+
+                    <div class="d-lg-flex align-items-center d-none ms-auto">
+                        <i class="navbar-icon bi-telephone-plus me-3"></i>
+                        <a class="custom-btn btn" href="#section_5">
+                            +254798226640
+                        </a>
+                    </div>
+                </div>
+
             </div>
-         </div>
-      </div>
-      <!-- Banner Section End -->
-   </div>
-   <!-- Header Section End -->
+        </nav>
 
-   <!-- Footer Section Start -->
-   <div class="copyright_section" style="display: flex; justify-content: center; align-items: center; text-align: left; padding: 20px 0;">
-      <div class="container">
-         <p class="copyright_text" style="margin-bottom: 5px; font-size: 18px; font-weight: bold; text-decoration: underline;">
-            Smart Ideas Portal - How it Works
-         </p>
-         <p class="copyright_text" style="margin-bottom: 5px;">1. Register then log in to your account</p>
-         <p class="copyright_text" style="margin-bottom: 5px;">2. Pay Ksh. 200 or 10 US Dollars</p>
-         <p class="copyright_text" style="margin-bottom: 5px;">3. Post your business idea</p>
-         <p class="copyright_text" style="margin-top: 5px;">4. Sitback & wait to get noticed by our investors</p>
-      </div>
-   </div>
-   <!-- Footer Section End -->
+        <main>
 
-   <!-- Javascript Files -->
-   <script src="js/jquery.min.js"></script>
-   <script src="js/popper.min.js"></script>
-   <script src="js/bootstrap.bundle.min.js"></script>
-   <script src="js/jquery-3.0.0.min.js"></script>
-   <script src="js/plugin.js"></script>
-   <!-- Sidebar -->
-   <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
-   <script src="js/custom.js"></script>
-   <!-- Javascript -->
-   <script src="js/owl.carousel.js"></script>
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
-</body>
+            <section class="hero d-flex justify-content-center align-items-center" id="section_1">
+                <div class="container">
+                    <div class="row">
 
+                        <div class="col-lg-7 col-12">
+                            <div class="hero-text">
+
+                                <h3 class="mb-4">Do you have a good business idea & you're in need of cash or business partner?</h3>
+                                <p class="mb-4"><a class="custom-btn btn custom-link" href="#section_2">Get started</a></p>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-5 col-12 position-relative">
+                            <div class="hero-image-wrap"></div>
+                            <img src="images/portrait-happy-excited-man-holding-laptop-computer.png" class="hero-image img-fluid" alt="">
+                        </div>
+
+                    </div>
+                </div>
+
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#535da1" fill-opacity="1" d="M0,160L24,160C48,160,96,160,144,138.7C192,117,240,75,288,64C336,53,384,75,432,106.7C480,139,528,181,576,208C624,235,672,245,720,240C768,235,816,213,864,186.7C912,160,960,128,1008,133.3C1056,139,1104,181,1152,202.7C1200,224,1248,224,1296,197.3C1344,171,1392,117,1416,90.7L1440,64L1440,0L1416,0C1392,0,1344,0,1296,0C1248,0,1200,0,1152,0C1104,0,1056,0,1008,0C960,0,912,0,864,0C816,0,768,0,720,0C672,0,624,0,576,0C528,0,480,0,432,0C384,0,336,0,288,0C240,0,192,0,144,0C96,0,48,0,24,0L0,0Z"></path></svg>
+            </section>
+
+
+            <section class="about section-padding" id="section_2">
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-lg-6 col-12">
+                            <img src="images/Goxlog Global final_page-0001.jpg" class="about-image img-fluid" alt="">
+                        </div>
+
+                        <div class="col-lg-6 col-12 mt-5 mt-lg-0">
+                            <div class="about-thumb">
+
+                                <div class="section-title-wrap d-flex justify-content-center align-items-center mb-4">
+                                    <h3 class="text-white me-4 mb-0">Who we are</h3>
+                                </div>
+
+                                <p>Welcome to Goxlog, the global marketplace for business ideas! At Goxlog, we believe that every great business starts with a bold idea, and we’re here to connect entrepreneurs, innovators, and business enthusiasts from around the world. </p> 
+                                <p>Our platform is designed to empower individuals to share their business ideas, seek funding, or find the perfect partnership to bring their vision to life. </p>
+                                <p>Whether you’re an aspiring entrepreneur with a groundbreaking concept or an established business professional looking to collaborate, Goxlog provides a dynamic space to showcase your ideas, gain visibility, and connect with like-minded individuals. Users can explore a wide range of ideas, show their support by liking posts, and even discover opportunities to sponsor or fund the most promising ventures. </p>
+                                <p>At Goxlog, we’re passionate about fostering innovation and collaboration. Our mission is to create a global community where creativity meets opportunity, and where the best ideas have the chance to thrive. Join us today and be part of a platform that’s shaping the future of business, one idea at a time. Together, let’s turn ideas into reality!</p>
+             
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+            <section class="contact section-padding" id="section_5">
+                    <div class="container">
+                        <div class="row">
+
+                            <div class="col-lg-6 col-md-8 col-12">
+                                <div class="section-title-wrap d-flex justify-content-center align-items-center mb-5">
+
+                                    <h3 class="text-white ms-4 mb-0">Contact us</h3>
+                                </div>
+                            </div>
+
+                            <div class="clearfix"></div>
+
+                            <div class="col-lg-3 col-md-6 col-12 pe-lg-0">
+                                <div class="contact-info contact-info-border-start d-flex flex-column">
+                                    <strong class="site-footer-title d-block mt-4 mb-3">Stay connected</strong>
+
+                                    <ul class="social-icon">
+                                        <li class="social-icon-item"><a href="#" class="social-icon-link bi-twitter"></a></li>
+
+                                        <li class="social-icon-item"><a href="#" class="social-icon-link bi-instagram"></a></li>
+
+                                        <li class="social-icon-item"><a href="#" class="social-icon-link bi-pinterest"></a></li>
+
+                                        <li class="social-icon-item"><a href="#" class="social-icon-link bi-youtube"></a></li>
+                                    </ul>
+
+                                    <strong class="site-footer-title d-block mt-4 mb-3">Reach Out</strong>
+
+                                    <p class="mb-0">In case of any enquiries, feel free to call or send an email</p>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-3 col-md-6 col-12 ps-lg-0">
+                                <div class="contact-info d-flex flex-column">
+
+                                    <strong class="site-footer-title d-block mt-4 mb-3">Email</strong>
+
+                                    <p>
+                                        <a href="mailto:hello@josh.design">
+                                            enquiries@goxlog.com
+                                        </a>
+                                    </p>
+
+                                    <strong class="site-footer-title d-block mt-4 mb-3">Call</strong>
+
+                                    <p class="mb-0">
+                                        <a href="tel: 120-240-9600">
+                                            +2547 9822 6640
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6 col-12 mt-5 mt-lg-0">
+                                <form action="#" method="get" class="custom-form contact-form" role="form">
+                                    <div class="row">
+                                        <div class="col-lg-6 col-md-6 col-12">
+                                            <div class="form-floating">
+                                                <input type="text" name="name" id="name" class="form-control" placeholder="Name" required="">
+                                                
+                                                <label for="floatingInput">Name</label>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-lg-6 col-md-6 col-12"> 
+                                            <div class="form-floating">
+                                                <input type="email" name="email" id="email" pattern="[^ @]*@[^ @]*" class="form-control" placeholder="Email address" required="">
+                                                
+                                                <label for="floatingInput">Email address</label>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-lg-12 col-12">
+                                            <div class="form-floating">
+                                                <textarea class="form-control" id="message" name="message" placeholder="Tell me about the project"></textarea>
+                                                
+                                                <label for="floatingTextarea">Tell me about the project</label>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-lg-3 col-12 ms-auto">
+                                            <button type="submit" class="form-control">Send</button>
+                                        </div>
+
+                                    </div>
+                                </form>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </main>
+
+        <footer class="site-footer">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-12 col-12">
+                        <div class="copyright-text-wrap">
+                            <p class="mb-0">
+                                <span class="copyright-text">Copyright © 2025 <a href="#">Goxlog global</a> Company. All rights reserved.</span>
+                                Design: 
+                                <a rel="sponsored" href="https://templatemo.com" target="_blank">Pinelink solutions</a>
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </footer>
+
+        <!-- JAVASCRIPT FILES -->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.sticky.js"></script>
+        <script src="js/click-scroll.js"></script>
+        <script src="js/jquery.magnific-popup.min.js"></script>
+        <script src="js/magnific-popup-options.js"></script>
+        <script src="js/custom.js"></script>
+
+    </body>
 </html>
