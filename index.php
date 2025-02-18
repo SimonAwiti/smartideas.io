a9231b31edffb5b9484ee9eb11e091172b95b02a
simonawiti <awitisimon23@gmail.com>
2025-02-18 13:27:57 +0100

    final touches on the index page

--- index.php
+++ index.php
@@ -3,17 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <meta name="description" content="">
         <meta name="author" content="Simon Awiti">
-
         <title>Goxlog Global</title>
-
         <!-- CSS FILES -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
-
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/bootstrap-icons.css" rel="stylesheet">
         <link href="css/magnific-popup.css" rel="stylesheet">
@@ -21,23 +17,21 @@
     </head>
     
     <body>
-
         <section class="preloader">
             <div class="spinner">
                 <span class="spinner-rotate"></span>    
             </div>
         </section>
 
-                <nav class="navbar navbar-expand-lg">
+        <nav class="navbar navbar-expand-lg">
             <div class="container">
-
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <!-- Logo added here -->
                 <a href="index.html" class="navbar-brand mx-auto mx-lg-0">
-                    <h5 style="color:white;"><img src="images/logo.png" alt="Goxlog Logo" style="height: 100px;"> Goxlog Global</h5>
+                    <h5 style="color:#b92504;"><img src="images/logo.png" alt="Goxlog Logo" class="logo-img" style="height: 80px;"> Goxlog Global</h5>
                 </a>
 
                 <div class="collapse navbar-collapse" id="navbarNav">
@@ -46,15 +40,12 @@
                         <li class="nav-item">
                             <a class="nav-link click-scroll" href="#section_1">Home</a>
                         </li>
-
                         <li class="nav-item">
                             <a class="nav-link click-scroll" href="#section_2">About</a>
                         </li>
-
                         <li class="nav-item">
                             <a class="nav-link click-scroll" href="login.html">Login</a>
                         </li>
-
                         <li class="nav-item">
                             <a class="nav-link click-scroll" href="#section_5">Contact</a>
                         </li>
@@ -103,8 +94,13 @@
                                 </div>
                         </div>
                         <div class="col-lg-6 col-12">
-                        <p>Welcome to Goxlog, the global marketplace for business ideas! At Goxlog, we believe that every great business starts with a bold idea, and we’re here to connect entrepreneurs, innovators, and business enthusiasts from around the world. </p> 
-                        <p>Our platform is designed to empower individuals to share their business ideas, seek funding, or find the perfect partnership to bring their vision to life. Whether you’re an aspiring entrepreneur with a groundbreaking concept or an established business professional looking to collaborate, Goxlog provides a dynamic space to showcase your ideas, gain visibility, and connect with like-minded individuals. Users can explore a wide range of ideas, show their support by liking posts, and even discover opportunities to sponsor or fund the most promising ventures. </p>
+                            
+                        <div class="col-lg-6 col-md-8 col-12">
+                            <div class="section-title-wrap d-flex justify-content-center align-items-center mb-5">
+                                <h3 class="text-white ms-4 mb-0">About Us</h3>
+                            </div>
+                        </div>
+                        <p>Welcome to Goxlog, the global marketplace for business ideas! At Goxlog, we believe that every great business starts with a bold idea, and we’re here to connect entrepreneurs, innovators, and business enthusiasts from around the world. Our platform is designed to empower individuals to share their business ideas, seek funding, or find the perfect partnership to bring their vision to life. Whether you’re an aspiring entrepreneur with a groundbreaking concept or an established business professional looking to collaborate, Goxlog provides a dynamic space to showcase your ideas, gain visibility, and connect with like-minded individuals. Users can explore a wide range of ideas, show their support by liking posts, and even discover opportunities to sponsor or fund the most promising ventures. </p>
                         <p>At Goxlog, we’re passionate about fostering innovation and collaboration. Our mission is to create a global community where creativity meets opportunity, and where the best ideas have the chance to thrive. Join us today and be part of a platform that’s shaping the future of business, one idea at a time. Together, let’s turn ideas into reality!</p>
                         </div>
 
